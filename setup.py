24b322ece2b19dca8f1605ba750377b48bc02227
peterbaudains <98837192+peterbaudains@users.noreply.github.com>
2023-09-20 14:46:12 +0100

    Update setup for 1.1.0b1

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name = 'vizent',
+    name = 'vizent-beta',
     packages = ['vizent'],
     package_data={'vizent': ['images/*.png']},
     include_package_data = True,
-    version = '1.1.0',
+    version = '1.1.0b1',
     license='MIT',
-    description = 'A library for creating scatterplots using visual entropy glyphs.',
+    description = 'A python library for visualising bivariate data using glyphs and node-link diagrams with matplotlib.',
     author = 'CUSP London and Lucy McLaughlin',
     author_email = 'lucy.mclaughlin@ncl.ac.uk',
     url = 'https://github.com/cusp-london/vizent',
-    keywords = ['visualization', 'plot', 'bivariate', 'glyphs', 'scatterplot', 'dataviz'],
+    keywords = ['visualization', 'plot', 'bivariate', 'glyphs', 'scatterplot', 'dataviz', 'networks', 'uncertainty'],
     install_requires = ['matplotlib', 'numpy', 'scipy'],
     extras_require = {
         'background_map': ['cartopy'],
