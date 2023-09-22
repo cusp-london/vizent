5087aaba417695f299d454f32ba8d785faabd33c
peterbaudains <98837192+peterbaudains@users.noreply.github.com>
2023-09-22 11:08:31 +0100

    Revert package name in setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name = 'vizent-beta',
+    name = 'vizent',
     packages = ['vizent'],
     package_data={'vizent': ['images/*.png']},
     include_package_data = True,
